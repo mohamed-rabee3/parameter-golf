a9bf987f3a66b1ab8b6fcdb17e87d05b30b87981
Mohamed Rabiee <mohamed.rabiee03@gmail.com>
2026-03-25 21:26:06 +0200

    .

--- train_gpt_v2.py
+++ train_gpt_v2.py
@@ -24,7 +24,10 @@ import torch.distributed as dist
 import torch.nn.functional as F
 from torch import Tensor, nn
 from torch.nn.parallel import DistributedDataParallel as DDP
-from flash_attn_interface import flash_attn_func as flash_attn_3_func
+try:
+    from flash_attn_interface import flash_attn_func as flash_attn_3_func
+except ImportError:
+    flash_attn_3_func = None  # FA3 not available; not used in current code path
 class Hyperparameters:
     data_path = os.environ.get("DATA_PATH", "./data/datasets/fineweb10B_sp1024")
     train_files = os.path.join(data_path, "fineweb_train_*.bin")
